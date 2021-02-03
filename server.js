32eb1c908eda96794fa908057e18974903bb6edb
Patryk <musial.patryk10@gmail.com>
2021-02-04 00:07:26 +0100

    Add default local database string as fallback

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+if (process.env.DATABASE_NAME == null){
+    console.log("No database string provided. Using default - mongodb://127.0.0.1:27017")
+}
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE || 'mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (err) => { if(err) console.log(err) } )
 db.once('open', () => console.log('Successfully connected.'))
