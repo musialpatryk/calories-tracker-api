63f5a1e1ea21be4f4801e18ff6b351e9b1192841
Patryk <musial.patryk10@gmail.com>
2021-02-13 22:07:45 +0100

    Change default user data in init script

--- init.js
+++ init.js
@@ -3,9 +3,9 @@ const User = require("./models/user");
 
 module.exports = async () => {
   if ((await User.find()).length == 0) {
-    console.log("No users found. Created default user user.");
+    console.log("No users found. Created default user (user, p@ssword).");
     try {
-      const hashedPassword = await bcrypt.hash("user", 10);
+      const hashedPassword = await bcrypt.hash("p@ssword", 10);
       const newUser = new User({
         username: "user",
         password: hashedPassword,
