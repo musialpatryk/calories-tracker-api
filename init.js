0b091154cfcb909ef4e7e30f7b6fa5f89fdaf4d9
Patryk <musial.patryk10@gmail.com>
2021-02-13 01:00:37 +0100

    Add init script that check if users in db exists, if there no users add default one

---
+++ init.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
+
+module.exports = async () => {
+  if ((await User.find()).length == 0) {
+    console.log("No users found. Created default user user.");
+    try {
+      const hashedPassword = await bcrypt.hash("user", 10);
+      const newUser = new User({
+        username: "user",
+        password: hashedPassword,
+      });
+      await newUser.save();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-if (process.env.DATABASE_NAME == null){
-    console.log("No database string provided. Using default - mongodb://127.0.0.1:27017")
+if (process.env.DATABASE_NAME == null) {
+  console.log(
+    "No database string provided as ENV variable. Using default - mongodb://127.0.0.1:27017"
+  );
 }
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE || 'mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', (err) => { if(err) console.log(err) } )
-db.once('open', () => console.log('Successfully connected.'))
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE || "mongodb://127.0.0.1:27017", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const usersRouter = require('./routes/users')
-app.use('/api/users', usersRouter)
+const db = mongoose.connection;
+db.on("error", (err) => {
+  if (err) console.log(err);
+});
+const init = require("./init");
+db.once("open", () => {
+  console.log("Successfully connected.");
+  init();
+  if (process.env.ACCESS_TOKEN_SECRET == null)
+    console.log(
+      "No secret token provided as ENV variable. Provide ACCESS_TOKEN_SECRET key in .env file and restart server."
+    );
+});
 
-const daysRouter = require('./routes/days')
-app.use('/api/days', daysRouter)
+const usersRouter = require("./routes/users");
+app.use("/api/users", usersRouter);
 
-const productsRouter = require('./routes/products')
-app.use('/api/products', productsRouter)
+const daysRouter = require("./routes/days");
+app.use("/api/days", daysRouter);
 
-app.listen(process.env.PORT || 3000)
+const productsRouter = require("./routes/products");
+app.use("/api/products", productsRouter);
+
+app.listen(process.env.PORT || 3000);
